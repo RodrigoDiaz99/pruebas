3d6c395be6d5b08ad90921227a2293ca2d5d3446
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-03-20 00:14:44 -0600

    u

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -11,15 +11,15 @@
   <title>Landing Page - Start Bootstrap Theme</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
-  <link href="{{asset('vendor/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/vendor/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
   <!-- Custom styles for this template -->
-  <link href="{{asset('css/landing-page.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/resources/css/landing-page.min.css')}}" rel="stylesheet">
 
 </head>
 
@@ -222,8 +222,8 @@
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-  <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
 </body>
 
