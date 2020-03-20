40224651b6b78f081e1c63fff529b064c4114846
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-03-19 23:40:34 -0600

    u

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,14 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="{{asset('https://fonts.googleapis.com/css?family=Nunito:200,600')}}" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{asset('/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
   <link href="" rel="stylesheet">
-  <link href="{{asset('vendor/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet" rel="stylesheet" type="text/css">
+  <link href="{{asset('/vendor/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet" rel="stylesheet" type="text/css">
   <!--<link href="../.../vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">-->
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
