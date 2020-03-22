ca25f23a3cda56fdbf9e4cc33f719d26bb387d21
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-03-21 19:16:42 -0600

    u

--- resources/views/layouts/appl.blade.php
+++ resources/views/layouts/appl.blade.php
@@ -9,25 +9,25 @@
     <title>{{ config('app.name', 'Prueba') }}</title>
 
     <!-- Custom fonts for this theme -->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{!! asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
     <!-- Theme CSS -->
-    <link href="{{asset('css/freelancer.min.css')}}" rel="stylesheet">
+    <link href="{!! asset('css/freelancer.min.css') !!}" rel="stylesheet">
     <!-- Bootstrap core JavaScript -->
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{!! asset('vendor/jquery/jquery.min.js') !!}"></script>
+    <script src="{!! asset('vendor/bootstrap/js/bootstrap.bundle.min.js') !!}"></script>
 
     <!-- Plugin JavaScript -->
-    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{!! asset('vendor/jquery-easing/jquery.easing.min.js') !!}"></script>
 
     <!-- Contact Form JavaScript -->
-    <script src="{{ asset('js/jqBootstrapValidation.js') }}"></script>
-    <script src="{{ asset('js/contact_me.js') }}"></script>
+    <script src="{!! asset('js/jqBootstrapValidation.js') !!}"></script>
+    <script src="{!! asset('js/contact_me.js') !!}"></script>
 
     <!-- Custom scripts for this template -->
-    <script src="{{ asset('js/freelancer.min.js') }}"></script>
+    <script src="{!! asset('js/freelancer.min.js') !!}"></script>
    <style>
 
     </style>
