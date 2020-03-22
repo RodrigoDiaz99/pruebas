9b34e3567b967512d4f55b1f4a64cb3b94a4adec
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-03-21 19:19:20 -0600

    u

--- resources/views/layouts/appl.blade.php
+++ resources/views/layouts/appl.blade.php
@@ -9,7 +9,7 @@
     <title>{{ config('app.name', 'Prueba') }}</title>
 
     <!-- Custom fonts for this theme -->
-    <link href="{!! asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{!! asset('vendor/fontawesome-free/css/all.min.css') !!}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
