184b9039dc37029be03f9ebfc87cd6c67df5f0ad
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-03-22 20:50:48 -0600

    u

--- resources/views/layouts/appl.blade.php
+++ resources/views/layouts/appl.blade.php
@@ -25,12 +25,13 @@
             <div class="container">
                 <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+
                         <div class="flex-center position-ref full-height">
+                            <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
+                                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
+                                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
                             @if (Route::has('login'))
                                 <div class="top-right links">
                                     @auth
@@ -38,9 +39,7 @@
                                     @else
                                         <a href="{{ route('login') }}">Login</a>
 
-                                        @if (Route::has('register'))
-                                            <a href="{{ route('register') }}">Register</a>
-                                        @endif
+
                                     @endauth
                                 </div>
                             @endif
