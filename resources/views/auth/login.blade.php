5b4a9bf7cf37e1944e4089076c4f8bcd3f63fb4e
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-03-23 03:27:00 -0600

    u

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,10 +6,10 @@
     <div class="d-flex justify-content-center h-100">
         <!--<div class="col-md-8">-->
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+               <!-- <div class="card-header">{{ __('Login') }}</div>-->
                 <h3>Sign In</h3>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('') }}">
                         @csrf
 
                         <div class="form-group row">
