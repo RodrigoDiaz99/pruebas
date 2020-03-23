477743a8f11c5d725f2c324da27fd9b8ca78666b
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-03-23 02:44:40 -0600

    u

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
     <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/app2.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
