7ad164b6ca777e1ce9b72ac02924ac0455350ce4
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-03-23 01:22:35 -0600

    u

--- resources/views/layouts/appl.blade.php
+++ resources/views/layouts/appl.blade.php
@@ -38,7 +38,7 @@
                                     <a class="nav-link js-scroll-trigger" href="{{ url('/principal') }}">Home</a>
 
                                     @else
-                                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Acceso</a></li>
+                                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ route('log') }}">Acceso</a></li>
 
                                     @endauth
                                 </div>
