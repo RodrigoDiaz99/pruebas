c65dba40a33102191569f7913d75d6f19a500938
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-03-23 01:44:34 -0600

    u

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -113,48 +113,17 @@ margin-left: 4px;
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
 			<div class="card-header">
-				<h3>Sign In</h3>
+				<h3>Acceder</h3>
 				<div class="d-flex justify-content-end social_icon">
 
 				</div>
 			</div>
-			<div class="card-body">
-				<form>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input type="text" class="form-control" placeholder="username">
-
-					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
-						</div>
-						<input type="password" class="form-control" placeholder="password">
-					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
-					</div>
-				</form>
-			</div>
-			<div class="card-footer">
-
-
-			</div>
-		</div>
-	</div>
-</div>
-@section('content')
+			@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
@@ -210,6 +179,11 @@ margin-left: 4px;
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+
+
+
+
+
                             </div>
                         </div>
                     </form>
