8f08e8ecece20b14630680f161b11d2ed3f0663e
KennAyala <kennayala@outlook.com>
2020-03-23 20:15:05 -0600

    admin

--- resources/views/controlpanel/navigation/sidebar.blade.php
+++ resources/views/controlpanel/navigation/sidebar.blade.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Carmen Andrea Reyes Reyes - Dashboard</title>
-    <!-- Tell the browser to be responsive to screen width -->
+     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
