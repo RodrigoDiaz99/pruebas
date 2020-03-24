5090688994dae1d0c6bd3f9df36d31bc2c235ffb
KennAyala <kennayala@outlook.com>
2020-03-23 19:53:55 -0600

    admin

--- resources/views/controlpanel/navigation/sidebar.blade.php
+++ resources/views/controlpanel/navigation/sidebar.blade.php
@@ -78,14 +78,30 @@
                         </ul>
                     </li>
 
-                    <li class="nav-item">
-                        <a href="/pacientes" class="nav-link">
-                            <i class="nav-icon fas fa-table"></i>
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="#" class="nav-link active">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
-                                Lista de pacientes
+                                Pacientes
+                                <i class="right fas fa-angle-left"></i>
                             </p>
                         </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="/pacientes" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Registrar paciente</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/registro-paciente" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Lista de paciente</p>
+                                </a>
+                            </li>
+                        </ul>
                     </li>
+ 
                     <li class="nav-item has-treeview">
                         <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-copy"></i>
