e93c1debc9d9d9cb1a913d2f83cdbcc584acfdaf
KennAyala <kennayala@outlook.com>
2020-03-23 22:30:05 -0600

    agenda

--- resources/views/controlpanel/navigation/sidebar.blade.php
+++ resources/views/controlpanel/navigation/sidebar.blade.php
@@ -57,7 +57,7 @@
                         <a href="#" class="nav-link">
                             <i class="nav-icon  far fa-calendar-alt"></i>
                             <p>
-                                Citas
+                                Agenda
                                 <i class="right fas fa-angle-left"></i>
                             </p>
                         </a>
