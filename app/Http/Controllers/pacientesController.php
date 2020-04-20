0454f9b9210f987205c598422faae9a7ddcb8141
KennAyala <kennayala@outlook.com>
2020-04-20 15:27:54 -0500

    test

--- app/Http/Controllers/pacientesController.php
+++ app/Http/Controllers/pacientesController.php
@@ -12,9 +12,11 @@ class pacientesController extends Controller
     function cargaPacientes()
     {
 
-        //$cargarTabla = pacientesModel::all(); //carga toda la tabla
-        // $cargarTabla = pacientesModel::where('nombre','Kenn')->get(); //prueba de where, si funciona
-        $cargarTabla = pacientesModel::select('nombre')->get(); //solo cargar col "nombre"
+        // $cargarTabla = pacientesModel::all(); //carga toda la tabla, incluso sin usar
+        // $cargarTabla = pacientesModel::where('nombre','Kenn')->get(); //prueba de where
+        // $cargarTabla = pacientesModel::select('nombre')->get(); //solo cargar col "nombre" usando SELECT
+        $cargarTabla = pacientesModel::select('nombre', 'apep', 'apem', 'edad')->get(); //usando SELECT
+
         return view('expediente', ['tablaPaciente' => $cargarTabla]);
     }
 }
