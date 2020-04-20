be9f3068c75357032504b8afbd6287f7c82d98e5
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-04-19 20:43:28 -0500

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
-    public function show($id){
-       return 'UserID ' .$id;
+    public function show($name){
+      $user = \DB:table()
     }
 }
