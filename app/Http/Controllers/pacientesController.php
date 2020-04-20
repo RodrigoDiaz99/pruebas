81002cd89e0a47f1c894a27dbc061e7153052bdc
KennAyala <kennayala@outlook.com>
2020-04-20 15:25:04 -0500

    carga columna test

--- app/Http/Controllers/pacientesController.php
+++ app/Http/Controllers/pacientesController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\pacientesModel;
+
 class pacientesController extends Controller
 {
-    //
 
-    function cargaPacientes(){
-        
-        //$cargarTabla = pacientesModel::all();
-        $cargarTabla = pacientesModel::where('nombre','Kenn')->get();
-        return view('expediente', ['tablaPaciente'=>$cargarTabla]);
+    function cargaPacientes()
+    {
+
+        //$cargarTabla = pacientesModel::all(); //carga toda la tabla
+        // $cargarTabla = pacientesModel::where('nombre','Kenn')->get(); //prueba de where, si funciona
+        $cargarTabla = pacientesModel::select('nombre')->get(); //solo cargar col "nombre"
+        return view('expediente', ['tablaPaciente' => $cargarTabla]);
     }
 }
