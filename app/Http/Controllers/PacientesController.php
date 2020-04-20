5d1e058b877a1f51dc79f551fb0b01b6832e5d8e
KennAyala <kennayala@outlook.com>
2020-04-19 21:02:03 -0500

    db fix

--- app/Http/Controllers/PacientesController.php
+++ app/Http/Controllers/PacientesController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\pacientesModel;
 
-class Pacientes extends Controller
+class PacientesController extends Controller
 {
     //
 
