539700d0aa835500fe12bc54e05733281ed2e9e5
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-04-19 21:16:15 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::get('/calendario-analisis', function () {
 Route::get('/apertura-expediente', function () {
     return view('newexp');
 });
-Route::get('/consulta_expediente', 'PacientesController@consultaUsers'); {
+Route::get('/consulta_expediente', 'PacientesController@consultaUsers');
 
 Route::get('/db', 'PacientesController@consultaUsers');
 
