14bbab8f85ab623873ccb68c7e34f07a5963b03b
KennAyala <kennayala@outlook.com>
2020-05-20 18:05:27 -0500

    xd

---
+++ app/Http/Controllers/detallesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\pacientesModel;
+
+class pacientesController extends Controller
+{
+
+    function cargaDetalles()
+    {
+
+
+        $cargarTabla = pacientesModel::select('nombre_paciente', 'apellido_paterno', 'apellido_materno', 'edad')->get(); //usando SELECT
+
+        return view('detalleexpediente', ['tablaPaciente' => $cargarTabla]);
+    }
+}
