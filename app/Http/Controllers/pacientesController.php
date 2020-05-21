b2f96c88c1b01ffc6adf819b34cb82f52d70c633
KennAyala <kennayala@outlook.com>
2020-05-20 22:06:02 -0500

    asasdasd

--- app/Http/Controllers/pacientesController.php
+++ app/Http/Controllers/pacientesController.php
@@ -5,18 +5,33 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\pacientesModel;
+use App\detallesModel;
+
+
 
 class pacientesController extends Controller
 {
 
     function cargaPacientes()
-    {
+    { 
 
-        // $cargarTabla = pacientesModel::all(); //carga toda la tabla, incluso sin usar
+        // $casrgarTabla = pacientesModel::all(); //carga toda la tabla, incluso sin usar
         // $cargarTabla = pacientesModel::where('nombre','Kenn')->get(); //prueba de where
         // $cargarTabla = pacientesModel::select('nombre')->get(); //solo cargar col "nombre" usando SELECT
-        $cargarTabla = pacientesModel::select('nombre_paciente', 'apellido_paterno', 'apellido_materno', 'edad')->get(); //usando SELECT
+        $cargarTabla = pacientesModel::select('id_paciente', 'nombre_paciente', 'apellido_paterno', 'apellido_materno', 'edad')->get(); //usando SELECT
 
         return view('expediente', ['tablaPaciente' => $cargarTabla]);
     }
+    function cargaDetalles($id)
+    {
+
+
+        $cargarTabla = detallesModel::select('nombre_paciente', 'apellido_paterno', 'apellido_materno', 'edad'); //usando SELECT
+        $cargarTabla = detallesModel::where('id_paciente', $id)->get(); //usando SELECT
+
+
+        return view('detalleexp', ['tablaDetalles' => $cargarTabla]);
+    }
+
+
 }
