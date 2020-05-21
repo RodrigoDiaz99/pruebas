fc51d0a7735df7675159cd3a2c6aa92271472e0d
KennAyala <kennayala@outlook.com>
2020-05-20 21:45:54 -0500

    ahora si verga

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::get('/calendario-analisis', function () {
 Route::get('/apertura-expediente', function () {
     return view('aperturaexpediente');
 });
-Route::get('/consulta_expediente', 'pacientesController');
+Route::resource('/consulta_expediente', 'pacientesController');
 
 
 
