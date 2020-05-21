716a39519e4877f55b63b6a439ca24eceff865f1
KennAyala <kennayala@outlook.com>
2020-05-20 23:11:24 -0500

    que pendejo estoy

--- app/pacientesModel.php
+++ app/pacientesModel.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-//use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class pacientesModel extends Model
 {
     //use SoftDeletes;
+
+    protected $table = 'pacientes';
     protected $fillable = ['id_paciente'];
 }
