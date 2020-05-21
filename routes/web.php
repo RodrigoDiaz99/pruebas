e53ed8accf110544135adebe0bb48dfad69eb67a
KennAyala <kennayala@outlook.com>
2020-05-20 21:58:37 -0500

    conio arreglate por favor

--- routes/web.php
+++ routes/web.php
@@ -40,9 +40,6 @@ Route::get('/calendario-analisis', function () {
 Route::get('/apertura-expediente', function () {
     return view('aperturaexpediente');
 });
-
-Route::resource('/consulta_expediente', 'pacientesController');
-
-
+Route::get('/consulta_expediente', 'pacientesController@cargaPacientes');
 
 
