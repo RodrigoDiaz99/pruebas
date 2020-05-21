2388692b7b18cb96dc5b481336cea68d7e82ace9
KennAyala <kennayala@outlook.com>
2020-05-20 21:57:43 -0500

    malditaseaaaaa

--- app/Http/Controllers/pacientesController.php
+++ app/Http/Controllers/pacientesController.php
@@ -6,20 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\pacientesModel;
 
-
 class pacientesController extends Controller
 {
 
     function cargaPacientes()
-    { 
+    {
 
-        // $casrgarTabla = pacientesModel::all(); //carga toda la tabla, incluso sin usar
+        // $cargarTabla = pacientesModel::all(); //carga toda la tabla, incluso sin usar
         // $cargarTabla = pacientesModel::where('nombre','Kenn')->get(); //prueba de where
         // $cargarTabla = pacientesModel::select('nombre')->get(); //solo cargar col "nombre" usando SELECT
-        $cargarTabla = pacientesModel::select('id_paciente', 'nombre_paciente', 'apellido_paterno', 'apellido_materno', 'edad')->get(); //usando SELECT
+        $cargarTabla = pacientesModel::select('nombre_paciente', 'apellido_paterno', 'apellido_materno', 'edad')->get(); //usando SELECT
 
         return view('expediente', ['tablaPaciente' => $cargarTabla]);
     }
-
-
 }
