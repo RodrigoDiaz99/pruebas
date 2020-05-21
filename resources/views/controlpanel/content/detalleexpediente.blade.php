a260fefa05cddb19f9b00908f3e6a9fb3481d5f8
KennAyala <kennayala@outlook.com>
2020-05-20 23:18:38 -0500

    s

--- resources/views/controlpanel/content/detalleexpediente.blade.php
+++ resources/views/controlpanel/content/detalleexpediente.blade.php
@@ -19,7 +19,7 @@
                         <!-- text input -->
                         <div class="form-group">
                             <label>Nombre(s)</label>
-                            <input type="text" class="form-control" value="{{$pacientes->id_paciente}}">
+                            <input type="text" class="form-control" value="{{$paciente->id_paciente}}">
                         </div>
                     </div>
                     <div class="col-sm-6">
