b1868dd05f6e06f19222ee936c16804a3a599fd5
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-06-13 11:39:13 -0500

    update

--- app/Http/Controllers/agendaController.php
+++ app/Http/Controllers/agendaController.php
@@ -8,10 +8,7 @@ use App\agendaModel;
 
 class agendaController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     /**
      * Display a listing of the resource.
