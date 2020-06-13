a55c20e4a14dc2951dabd1b6c0391122bb942bba
KennAyala <kennayala@outlook.com>
2020-06-13 10:34:48 -0500

    asdab

--- app/Http/Controllers/pacientesController.php
+++ app/Http/Controllers/pacientesController.php
@@ -55,7 +55,7 @@ class pacientesController extends Controller
         pacientesModel::insert($datosPaciente);
  
 
-        return redirect()->route('lista-pacientes/index');
+        return redirect('lista-pacientes');
 
     }
 
