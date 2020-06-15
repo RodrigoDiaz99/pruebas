48151b99ac44ec0116f2a4688467c34828fa888f
KennAyala <kennayala@outlook.com>
2020-06-15 10:19:22 -0500

    consultar expediente

--- resources/views/controlpanel/content/detalleexpediente.blade.php
+++ resources/views/controlpanel/content/detalleexpediente.blade.php
@@ -1,4 +1,4 @@
- <body class="hold-transition sidebar-mini">
+ <body  class="hold-transition sidebar-mini">
      <div class="content-wrapper">
          <div class="card card-primary">
              <div class="card-header">
@@ -53,134 +53,121 @@
                          <div class="col-md-6">
                              <div class="form-group">
                                  <label>Estado</label>
-                                 <select class="form-control" style="width: max-content;">
+                                 <select disabled class="form-control" style="width: max-content;">
                                      <option selected="selected">Yucatán</option>
                                      <option>Alaskaguamas</option>
                                  </select>
                              </div>
                          </div>
                      
-                     </div>
-                     <div class="row">
-                         <div class="col-sm-6">
-                             <div class="form-group clearfix">
-                                 <label for="radioDiabetes">
-                                     Diabetes
-                                 </label>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioDiabetesSi" name="r1">
-                                     <label for="radioDiabetesSi"> Sí
-                                     </label>
-                                 </div>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioDiabetesNo" name="r1" checked>
-                                     <label for="radioDiabetesNo">No
-                                     </label>
-                                 </div>
-                             </div>
-                         </div>
-                         <div class="col-sm-6">
-                             <div class="form-group clearfix">
-                                 <label for="radioTBP">
-                                     TB. Pulmonar
-                                 </label>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioTBPSi" name="r2">
-                                     <label for="radioTBPSi"> Sí
-                                     </label>
-                                 </div>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioTBPNo" name="r2" checked>
-                                     <label for="radioTBPNo">No
-                                     </label>
-                                 </div>
-                             </div>
-                         </div>
-                         <div class="col-sm-6">
-                             <div class="form-group clearfix">
-                                 <label for="radioInterQ">
-                                     Inter. Quirúrgica
-                                 </label>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioInterQSi" name="r3">
-                                     <label for="radioInterQSi"> Sí
-                                     </label>
-                                 </div>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioInterQNo" name="r3" checked>
-                                     <label for="radioInterQNo">No
-                                     </label>
-                                 </div>
-                             </div>
-                         </div>
-                         <div class="col-sm-6">
-                             <div class="form-group clearfix">
-                                 <label for="radioPatP">
-                                     Patol. Psiquiátrica
-                                 </label>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioPatPSi" name="r4">
-                                     <label for="radioPatPSi"> Sí
-                                     </label>
-                                 </div>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioPatPNo" name="r4" checked>
-                                     <label for="radioPatPNo">No
-                                     </label>
-                                 </div>
-                             </div>
-                         </div>
-                         <div class="col-sm-6">
-                             <div class="form-group clearfix">
-                                 <label for="radioConvul">
-                                     Convulsiones </label>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioConvulSi" name="r5">
-                                     <label for="radioConvulSi"> Sí
-                                     </label>
-                                 </div>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioConvulNo" name="r5" checked>
-                                     <label for="radioConvulNo">No
-                                     </label>
-                                 </div>
-                             </div>
-                         </div>
-                         <!--  <div class="col-sm-6">
-                          <div class="form-group clearfix">
-                                 <label for="radioOtro">
-                                     Otros (Especifíque)
-                                 </label>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled class type="radio" id="radioOtroSi" name="r6">
-                                     <label for="radioOtroSi"> Sí
-                                     </label>
-                                 </div>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioOtroNo" name="r6" checked>
-                                     <label for="radioOtroNo">No
-                                     </label>
-                                 </div>
-                             </div> 
-                         </div> -->
-                         <div class="col-sm-6">
-                             <div class="form-group clearfix">
-                                 <label for="radioLueticas">
-                                     Lueticas
-                                 </label>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioLueticasSi" name="r7">
-                                     <label for="radioLueticasSi"> Sí
-                                     </label>
-                                 </div>
-                                 <div class="icheck-primary d-inline">
-                                     <input disabled type="radio" id="radioLueticasNo" name="r7" checked>
-                                     <label for="radioLueticasNo">No
-                                     </label>
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
+                        <!-- Inicio Radios-->
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group clearfix">
+                                    <label for="radioDiabetes">
+                                        Diabetes
+                                    </label>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioDiabetesSi" name="radioDiabetes" value="si">
+                                        <label for="radioDiabetesSi"> Sí
+                                        </label>
+                                    </div>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioDiabetesNo" name="radioDiabetes" value="no" checked>
+                                        <label for="radioDiabetesNo">No
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6">
+                                <div class="form-group clearfix">
+                                    <label for="radioTBP">
+                                        TB. Pulmonar
+                                    </label>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioTBPSi" name="radioTBP" value="si">
+                                        <label for="radioTBPSi"> Sí
+                                        </label>
+                                    </div>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioTBPNo" name="radioTBP" value="no" checked>
+                                        <label for="radioTBPNo">No
+                                        </label>
+                                    </div>
+                                </div>
+                                <!-- /.card -->
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group clearfix">
+                                    <label for="radioInterQ">
+                                        Inter. Quirúrgica
+                                    </label>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioInterQSi" name="radioInterQ" value="si">
+                                        <label for="radioInterQSi"> Sí
+                                        </label>
+                                    </div>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioInterQNo" name="radioInterQ" value="no" checked>
+                                        <label for="radioInterQNo">No
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6">
+                                <div class="form-group clearfix">
+                                    <label for="radioHTS">
+                                        Hipertensión
+                                    </label>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioHTSSi" name="radioHTS" value="si">
+                                        <label for="radioHTSSi"> Sí
+                                        </label>
+                                    </div>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioHTSNo" name="radioHTS" value="no" checked>
+                                        <label for="radioHTSNo">No
+                                        </label>
+                                    </div>
+                                </div>
+                                <!-- /.card -->
+                            </div>
+
+                            <div class="col-sm-6">
+                                <div class="form-group clearfix">
+                                    <label for="radioPatP">
+                                        Patol. Psiquiátrica
+                                    </label>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioPatPSi" name="radioPatP" value="si">
+                                        <label for="radioPatPSi"> Sí
+                                        </label>
+                                    </div>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioPatPNo" name="radioPatP" value="no" checked>
+                                        <label for="radioPatPNo">No
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group clearfix">
+                                    <label for="radioConvul">
+                                        Convulsiones </label>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioConvulSi" name="radioConv" value="si">
+                                        <label for="radioConvulSi"> Sí
+                                        </label>
+                                    </div>
+                                    <div class="icheck-primary d-inline">
+                                        <input disabled type="radio" id="radioConvulNo" name="radioConv" value="no" checked>
+                                        <label for="radioConvulNo">No
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
                      <div class="col-sm-6">
                          <div class="form-group">
                              <button onclick="verificarRadio()" type=" " class="btn btn-primary">Update</button>
@@ -188,11 +175,13 @@
                              <button type=" " class="btn btn-primary">Ver colposcopías</button>  -->
                          </div>
                      </div>
-                     <!--   <div class="col-sm-6">
+                     <!-- 
+                          <div class="col-sm-6">
                          <div class="form-group">
                              <button type=" " class="btn btn-danger">Cancelar</button>
                          </div>
-                     </div> -->
+                     </div>
+                    -->
              </div>
          </div>
      </div>
@@ -201,9 +190,74 @@
          @yield('footer')
      </footer>
      <script>
+
+$(document).ready(verificarRadio());
+
          function verificarRadio() {
-            var radiotest = {!! json_encode($antecedentesP->diabetes) !!};
-            console.log(radiotest);
+            //Tomar datos de la BD
+            var rDiabetes = {!! json_encode($antecedentesP->diabetes) !!};
+            var rQuiru = {!! json_encode($antecedentesP->interv_quirurgica) !!};
+            var rPatol = {!! json_encode($antecedentesP->patol_psiquiatrica) !!};
+            var rTuber = {!! json_encode($antecedentesP->tbpulmonar) !!};
+            var rHiper = {!! json_encode($antecedentesP->hipertencion) !!};
+            var rConvu = {!! json_encode($antecedentesP->convulsiones) !!};
+            
+            //Verificar datos recibidos
+            console.log(rDiabetes, rQuiru, rPatol, rTuber, rHiper, rConvu);
+
+            //Set de radios en la vista (comparar)
+
+            //Hipertensión
+            if(rHiper == 'si'){
+                rd = document.getElementById("radioDiabetesSi").checked = true;
+                console.log("chec");
+            }else{
+                rd = document.getElementById("radioDiabetesNo").checked = true;
+            }
+
+            //Intervención Quirú
+            if(rQuiru == 'si'){
+                rd = document.getElementById("radioInterQSi").checked = true;
+                console.log("chec");
+            }else{
+                rd = document.getElementById("radioInterQNo").checked = true;
+            }
+
+            //Patología Psi
+            if(rPatol == 'si'){
+                rd = document.getElementById("radioPatPSi").checked = true;
+                console.log("chec");
+            }else{
+                rd = document.getElementById("radioPatPNo").checked = true;
+            }
+
+            //Tuberculosis
+            if(rTuber == 'si'){
+                rd = document.getElementById("radioTBPSi").checked = true;
+                console.log("chec");
+            }else{
+                rd = document.getElementById("radioTBPNo").checked = true;
+            }
+
+            //Hipertensión
+
+            if(rHiper == 'si'){
+                rd = document.getElementById("radioHTSSi").checked = true;
+                console.log("chec");
+            }else{
+                rd = document.getElementById("radioHTSNo").checked = true;
+            }
+
+            //Convulsiones
+            if(rConvu == 'si'){
+                rd = document.getElementById("radioConvulSi").checked = true;
+                console.log("chec");
+            }else{
+                rd = document.getElementById("radioConvulNo").checked = true;
+            }
+
+
+
          }
 
          $(function() {
